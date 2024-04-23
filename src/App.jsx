97249e888f5a3e76c50e061e86c0a9f4bfb19e49
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-04-23 00:06:25 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { Container } from '@mui/material';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Book from './pages/Book';
-import FormSubmit from './pages/FormSubmit';
-import Error404 from './pages/Error404';
+// function App() {
 
-function App() {
-  return (
-    <>
-      <Container maxWidth="100" sx={{ background: '#3F51B5', height: 50 }} />
+//   return (
+//     <>
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/home/:id" element={<Book />} />
-          <Route path="/home/submitbook" element={<FormSubmit />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Navigate to="/home" />} />
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/home/:id" element={<Book />} />
+//           <Route path="/home/submitbook" element={<FormSubmit />} />
+//           <Route path="*" element={<Error404 />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
